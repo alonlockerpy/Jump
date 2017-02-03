af41fb1dabbe4a717023f41c3f933dc0f8f658b9
locke <alon.locker@gmail.com>
2017-02-03 17:56:31 +0200

    More functions that hendle files

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-s = "fname:John,lname:doe,mname:dunno,city:Florida"
-sd = dict(u.split(":") for u in s.split(","))
-for value in sd.values():
-    print "test: " + value
+#s = "fname:John,lname:doe,mname:dunno,city:Florida"
+#sd = dict(u.split(":") for u in s.split(","))
+#for value in sd.values():
+#    print "test: " + value
+
+
+def stringtest():
+    with open('test.txt') as file:
+        print "Here are your tasks:"
+        for line in file.readlines():
+            arry = line.split(";")
+            date = arry[0]
+            task = arry[2]
+            print date, task
+    file.close()
+
+stringtest()
+    #dic[arry[4]] = arry[0]
+    #dic with more then one value
+
